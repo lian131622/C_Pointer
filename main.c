6ce3d33504e27e3259716857005578e28bee9b90
Haolian <lian_john@163.com>
2021-09-03 14:06:18 -0400

    Add second pointer

--- main.c
+++ main.c
@@ -10,4 +10,17 @@ int main(){
     what_is_1D_array_name(b);
     printf("----\n");
     what_is_2D_array_name(c);
+    // second order pointer
+    // 不管是 int** p 还是 int **p 都是一个意思
+    int* p = &a;
+    int** q = &p;
+    printf("first pointer:%p\n",p);
+    printf("second pointer:%p\n",q);
+    int *p1 = &a;
+    int **q1 = &p;
+    printf("first pointer:%p\n",p1);
+    printf("second pointer:%p\n",q1);
+
+
+
 }
